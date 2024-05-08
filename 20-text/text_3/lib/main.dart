ae4d5a1185c2171f56c0deb052c8b5b6bc9faed6
rudri04 <therudripatel@gmail.com>
2024-05-08 11:06:47 +0530

    add

---
+++ 20-text/text_3/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child:Text("Good Morning",style: TextStyle(fontSize: 10,),),
+        
+      ),
+    ),
+    ),
+  );
+}
+
