43ff9b540a656847acb513ec9f1c178f6c92db22
rudri04 <therudripatel@gmail.com>
2024-06-14 14:13:39 +0530

    update

--- 22-textformfield/ex_2201/android/app/src/main/kotlin/com/example/ex_2201/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ex_2201
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- 22-textformfield/ex_2202/android/app/src/main/kotlin/com/example/ex_2202/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ex_2202
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
