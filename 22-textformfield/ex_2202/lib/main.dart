8370018f1abe044f2b5c1eb51793be0b5c4cda31
rudri04 <therudripatel@gmail.com>
2024-06-21 09:49:32 +0530

    update

---
+++ 22-textformfield/ex_2202/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: demo(),
+    );
+  }
+}
+
+class demo extends StatefulWidget {
+  const demo({super.key});
+
+  @override
+  State<demo> createState() => _demoState();
+}
+
+class _demoState extends State<demo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 164, 238, 252),
+        title: Text("Text Field Demo"),
+        
+      ),
+      body: Center(
+        child: Container(
+          child: TextFormField(
+            decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(width: 3,color: Color.fromARGB(255, 237, 93, 223))
+              ),
+              prefix: Icon(Icons.person_2),
+              hintText: "Enter UserName",
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
